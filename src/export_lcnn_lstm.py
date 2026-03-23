ccc147bfb0a8fcadaa6210b38c995890cf7d4be0
heixiayule <202522310930@std.uestc.edu.cn>
2026-03-23 19:06:15 +0800

    修改导出脚本名，删除test

--- src/export_lcnn_lstm.py
+++ src/export_lcnn_lstm.py
@@ -5,20 +5,20 @@ import tensorflow as tf
 import shutil
 import tf2onnx
 import onnx
-from model.lcnn import build_lcnn
+from model.lcnn_lstm import build_lcnn_lstm
 
 EXPORT_DIR = "../onnx"
 WIN_DIR = "/mnt/d/Documents/BaiduSyncdisk/WORKS/Deepfake2026/Deepfake/artifacts/onnx"
 
 BATCH = 1
-INPUT_SHAPE = (128, 128, 1)   # 如果你们确定了其他尺寸，可以在这里改
+INPUT_SHAPE = (128, 128, 1)   # 可修改
 
-model = build_lcnn(list(INPUT_SHAPE), n_label=2)
+model = build_lcnn_lstm(list(INPUT_SHAPE), n_label=2)
 spec = (tf.TensorSpec((BATCH, *INPUT_SHAPE), tf.float32, name="input"),)
 _ = model(tf.random.normal([BATCH, *INPUT_SHAPE]), training=False)
 
-out_path = os.path.join(EXPORT_DIR, "lcnn_test.onnx")
-win_path = os.path.join(WIN_DIR, "lcnn_test.onnx")
+out_path = os.path.join(EXPORT_DIR, "lcnn_lstm.onnx")
+win_path = os.path.join(WIN_DIR, "lcnn_lstm.onnx")
 onnx_model, _ = tf2onnx.convert.from_keras(
     model,
     input_signature=spec,
